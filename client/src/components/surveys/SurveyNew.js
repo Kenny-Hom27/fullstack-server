7c4346f7114d6ce5cf2aa95e3c2db63d4e89f495
github username <kenny.hom27@gmail.com>
2018-06-10 22:22:29 -0400

    finished course

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
+import { reduxForm } from 'redux-form'
 import SurveyForm from './SurveyForm'
+import SurveyFormReview from './SurveyFormReview'
 
 class SurveyNew extends Component {
+
+  state = {
+    showFormReview: false
+  }
+
   render() {
     return (
-      <SurveyForm/>
+      this.state.showFormReview ?
+      <SurveyFormReview onCancel={() => this.setState({ showFormReview : false})}/>
+      :
+      <SurveyForm onSurveySubmit={() => this.setState({ showFormReview : true})}/>
     )
   }
 }
 
-export default SurveyNew
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew)
